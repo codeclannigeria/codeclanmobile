d42c0950280a3a4715e61ca4e829e07b3a99aac1
Ajiboye Temitope <mrtayormi@outlook.com>
2020-05-01 22:49:21 +0100

    Home Design

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,103 @@
+import 'package:codeclanmobile/constants/constants.dart';
+import 'package:codeclanmobile/models/category.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.only(left: 20, top: 50, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SvgPicture.asset("assets/icons/menu.svg"),
+                Image.asset("assets/images/user.png"),
+              ],
+            ),
+            SizedBox(height: 30),
+            Text("Hey Fortune,", style: kHeadingextStyle),
+            Text("Find a course you want to learn", style: kSubheadingextStyle),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 30),
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFF5F5F7),
+                borderRadius: BorderRadius.circular(40),
+              ),
+              child: Row(
+                children: <Widget>[
+                  SvgPicture.asset("assets/icons/search.svg"),
+                  SizedBox(width: 16),
+                  Text(
+                    "Search for anything",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xFFA0A5BD),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text("Category", style: kTitleTextStyle),
+                Text(
+                  "See All",
+                  style: kSubtitleTextSyule.copyWith(color: kBlueColor),
+                ),
+              ],
+            ),
+            SizedBox(height: 30),
+            Expanded(
+              child: StaggeredGridView.countBuilder(
+                padding: EdgeInsets.all(0),
+                crossAxisCount: 2,
+                itemCount: categories.length,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: EdgeInsets.all(20),
+                    height: index.isEven ? 200 : 240,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      image: DecorationImage(
+                        image: AssetImage(categories[index].image),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          categories[index].name,
+                          style: kTitleTextStyle,
+                        ),
+                        Text(
+                          '${categories[index].numOfCourses} Courses',
+                          style: TextStyle(
+                            color: kTextColor.withOpacity(.5),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+                staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
