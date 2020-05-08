1a299cc3209daab146d819a46207313c179d6b5a
Ajiboye Temitope <mrtayormi@outlook.com>
2020-05-08 20:29:05 +0100

    Login and auth blocs

---
+++ lib/blocs/authentication_bloc.dart
@@ -0,0 +1,84 @@
+import 'package:codeclanmobile/repositories/repositories.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final UserRepository userRepository;
+
+  AuthenticationBloc({@required this.userRepository})
+      : assert(userRepository != null);
+
+  @override
+  AuthenticationState get initialState => AuthenticationUninitialized();
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AppStarted) {
+      final bool hasToken = await userRepository.hasToken();
+
+      if (hasToken) {
+        yield AuthenticationAuthenticated();
+      } else {
+        yield AuthenticationUnauthenticated();
+      }
+    }
+
+    if (event is LoggedIn) {
+      yield AuthenticationLoading();
+      await userRepository.persistToken(event.token);
+      yield AuthenticationAuthenticated();
+    }
+
+    if (event is LoggedOut) {
+      yield AuthenticationLoading();
+      await userRepository.deleteToken();
+      yield AuthenticationUnauthenticated();
+    }
+  }
+}
+
+/// Authentication Events
+
+abstract class AuthenticationEvent extends Equatable {
+  const AuthenticationEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AppStarted extends AuthenticationEvent {}
+
+class LoggedIn extends AuthenticationEvent {
+  final String token;
+
+  const LoggedIn({@required this.token});
+
+  @override
+  List<Object> get props => [token];
+
+  @override
+  String toString() => 'LoggedIn { token: $token }';
+}
+
+class LoggedOut extends AuthenticationEvent {}
+
+
+
+/// Authentication States
+/// This is used to maintain states of authentication
+abstract class AuthenticationState extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+class AuthenticationUninitialized extends AuthenticationState {}
+
+class AuthenticationAuthenticated extends AuthenticationState {}
+
+class AuthenticationUnauthenticated extends AuthenticationState {}
+
+class AuthenticationLoading extends AuthenticationState {}
